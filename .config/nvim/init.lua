1be7730cfb069b63875f1cc5bcac61799e83770c
Lionel Nkabi <nkabisto@gmail.com>
2025-11-05 03:08:23 +0200

    latest changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,6 @@
 -- init.lua - Optimized Neovim Configuration
+
+-- Lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -17,7 +19,7 @@ vim.g.loaded_2html_plugin = 1
 vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
 
--- Basic Settings (moved before plugins for faster UI)
+-- Basic Settings
 vim.opt.compatible = false
 vim.opt.number = true
 vim.opt.cursorline = true
@@ -41,10 +43,9 @@ vim.opt.autoindent = true
 vim.opt.expandtab = true
 vim.opt.encoding = 'utf-8'
 vim.opt.clipboard = "unnamedplus"
-
 vim.g.python_highlight_all = 1
 
--- Keymaps (set early so they're available)
+-- Keymaps
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>w', ':w<CR>')
 vim.keymap.set('n', '<leader>q', ':q<CR>')
@@ -57,32 +58,33 @@ vim.keymap.set('n', '<space>', 'za')
 vim.keymap.set('v', '<leader>y', '"+y', { desc = "Copy to system clipboard"})
 vim.keymap.set('n', '<leader>p', '"+p', { desc = "Paste from system clipboard"})
 
--- Plugin Configuration with Lazy Loading
-vim.g.ale_disable_lsp = 1 -- disables ALE's internal LSP client
+-- Plugin Configuration
+vim.g.ale_disable_lsp = 1
+
 require("lazy").setup({
-  -- Core UI (load immediately)
-  { 
-    'folke/tokyonight.nvim', 
-    priority = 1000, 
+  -- UI
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
     config = function()
       vim.cmd('colorscheme tokyonight-night')
-    end 
+    end
   },
-
-  { 
-    'vim-airline/vim-airline', 
+  {
+    'vim-airline/vim-airline',
     dependencies = {
-      {'vim-airline/vim-airline-themes', config = function()
-        vim.g.airline_theme = 'dark'
-      end}
+      { 'vim-airline/vim-airline-themes', config = function()
+          vim.g.airline_theme = 'dark'
+        end
+      }
     },
-    event = "UIEnter" -- Load after UI is ready
+    event = "UIEnter"
   },
 
-  -- File tree (load on command)
-  { 
-    'nvim-tree/nvim-tree.lua', 
-    dependencies = {'nvim-tree/nvim-web-devicons'},
+  -- File Tree
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     cmd = { "NvimTreeToggle", "NvimTreeFindFile", "NvimTreeOpen" },
     keys = {
       { "<C-n>", "<cmd>NvimTreeToggle<CR>", desc = "Toggle File Tree" },
@@ -93,235 +95,162 @@ require("lazy").setup({
     end
   },
 
-  -- LSP and completion (load on relevant events)
+  -- LSP and Completion
   {
     'neovim/nvim-lspconfig',
     event = "BufReadPre",
-    dependencies = { 
-      'hrsh7th/nvim-cmp',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',  
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
     },
     opts = {
       servers = {
         pyright = {},
-        ts_ls = {},
+        tsserver = {},
         html = {},
         cssls = {},
       },
     },
-  config = function(_, opts)
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    local lspconfig = require('lspconfig')
-    for server, config in pairs(opts.servers) do
-      config.capabilities = capabilities
-      lspconfig[server].setup(config)
+    config = function(_, opts)
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      for server, config in pairs(opts.servers) do
+        config.capabilities = capabilities
+        vim.lsp.config[server].setup(config)
+      end
     end
-  end
-},
+  },
 
-  -- Git (load on relevant events)
-  { 
-    'tpope/vim-fugitive', 
+  -- Git
+  {
+    'tpope/vim-fugitive',
     cmd = { "Git", "Gstatus", "Gcommit" },
     keys = {
       { "<leader>gs", "<cmd>Git<CR>", desc = "Git status" }
     }
   },
-
-  { 
-    'lewis6991/gitsigns.nvim', 
+  {
+    'lewis6991/gitsigns.nvim',
     event = "BufReadPre",
-    config = function() 
-      require('gitsigns').setup() 
-    end 
+    config = function()
+      require('gitsigns').setup()
+    end
   },
 
-  -- Tools (lazy loaded)
-  { 
-    'nvim-telescope/telescope.nvim', 
-    dependencies = {'nvim-lua/plenary.nvim'},
+  -- Telescope
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
     cmd = "Telescope",
     keys = {
       { '<C-p>', '<cmd>Telescope find_files<CR>', desc = "Find files" },
       { '<C-g>', '<cmd>Telescope live_grep<CR>', desc = "Live grep" },
       { '<leader>b', '<cmd>Telescope buffers<CR>', desc = "Buffers" },
     },
-    config = function() end -- config can be empty if just setting keymaps
+    config = function() end
   },
 
-  { 
-    'windwp/nvim-autopairs', 
+  -- Autopairs
+  {
+    'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = function()
       local autopairs = require('nvim-autopairs')
       autopairs.setup({})
       autopairs.remove_rule("'")
       autopairs.remove_rule('"')
-    end 
+    end
   },
 
-  { 
+  -- ALE
+  {
     'dense-analysis/ale',
     ft = { "python", "javascript", "typescript", "html", "css" },
     config = function()
       vim.g.ale_fixers = {
-        ['*'] = {'remove_trailing_lines', 'trim_whitespace'},
-        javascript = {'eslint', 'prettier'},
-        css = {'prettier'},
-        html = {'prettier'},
-        python = {'autopep8', 'yapf'},
+        ['*'] = { 'remove_trailing_lines', 'trim_whitespace' },
+        javascript = { 'eslint', 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        python = { 'autopep8', 'yapf' },
       }
       vim.g.ale_fix_on_save = 1
       vim.g.ale_python_autopep8_options = '--max-line-length=88'
       vim.g.ale_python_yapf_options = '--style={based_on_style: pep8, column_limit: 88}'
-    end 
+    end
   },
 
-  { 
-    'mattn/emmet-vim', 
+  -- Emmet
+  {
+    'mattn/emmet-vim',
     ft = { "html", "css", "javascript", "typescript", "javascriptreact", "typescriptreact" },
     config = function()
       vim.g.user_emmet_mode = 'n'
       vim.g.user_emmet_leader_key = ','
-    end 
+    end
   },
 
-  { 
-    'nvim-treesitter/nvim-treesitter', 
+  -- Treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     event = { "BufReadPost", "BufNewFile" },
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed = {"python", "javascript", "html", "css", "lua", "vim"},
+        ensure_installed = { "python", "javascript", "html", "css", "lua", "vim" },
         highlight = { enable = true },
       })
-    end 
-  },
-
-  -- Language-specific (load only for relevant filetypes)
-  { 
-    'tmhedberg/SimpylFold', 
-    ft = "python" 
-  },
-
-  { 
-    'Vimjas/vim-python-pep8-indent', 
-    ft = "python" 
-  },
-
-  -- Text objects and editing (load on first use)
-  { 
-    'tpope/vim-commentary', 
-    keys = { "gc", "gcc" } 
-  },
-
-  { 
-    'tpope/vim-surround', 
-    keys = { "cs", "ds", "ys" } 
-  },
-
-  { 
-    'lukas-reineke/indent-blankline.nvim', 
-    main = 'ibl', 
-    event = "BufReadPost",
-    config = function()
-      require('ibl').setup({})
-    end 
-  },
-
-  { 
-    'HiPhish/rainbow-delimiters.nvim',
-    event = "BufReadPost",
-    config = function()
-      -- Optional: add specific configuration if needed
     end
   },
 
-  -- Base plugins (load early)
+  -- Python-specific
+  { 'tmhedberg/SimpylFold', ft = "python" },
+  { 'Vimjas/vim-python-pep8-indent', ft = "python" },
+
+  -- Editing
+  { 'tpope/vim-commentary', keys = { "gc", "gcc" } },
+  { 'tpope/vim-surround', keys = { "cs", "ds", "ys" } },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    event = "BufReadPost",
+    config = function()
+      require('ibl').setup({})
+    end
+  },
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    event = "BufReadPost",
+    config = function() end
+  },
+
+  -- Base
   'tpope/vim-sensible',
 }, {
-  -- Lazy.nvim options for better performance
-  checker = { enabled = false }, -- Don't check for updates on startup
+  checker = { enabled = false },
   performance = {
     rtp = {
       disabled_plugins = {
-        "netrwPlugin",
-        "tutor",
-        "2html_plugin",
-        "matchit",
-        "matchparen",
+        "netrwPlugin", "tutor", "2html_plugin", "matchit", "matchparen",
       },
     },
   },
 })
 
--- Autocommands (moved after plugins)
+-- Autocommands
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
   callback = function()
-    vim.opt_local.formatoptions:remove({"c", "r", "o"})
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
 
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*.ejs",
   callback = function()
     vim.bo.filetype = "html"
   end,
 })
-
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = "*.py",
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.softtabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.wrap = true
-    vim.opt_local.linebreak = true
-    vim.opt_local.expandtab = true
-    vim.opt_local.autoindent = true
-    vim.opt_local.fileformat = "unix"
-  end,
-})
-
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = {"*.js", "*.html", "*.css", "*.jsx", "*.ts", "*.tsx"},
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.expandtab = true
-  end,
-})
-
-vim.cmd [[ highlight BadWhitespace ctermbg=red guibg=red ]]
-vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  pattern = {"*.py", "*.pyw", "*.c", "*.h", "*.js", "*.html", "*.css"},
-  callback = function()
-    vim.cmd [[ match BadWhitespace /\s\+$/ ]]
-  end,
-})
-
--- LSP keymaps (only set up when LSP attaches)
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  end,
-})
-
--- Syntax and filetype (at the end)
-vim.cmd('syntax on')
-vim.cmd('filetype plugin indent on')
